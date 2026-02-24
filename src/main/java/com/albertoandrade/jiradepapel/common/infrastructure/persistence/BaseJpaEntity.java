1ea5dfaee7be1be9a7d8dcd73b6d3e674fe040bf
TOTI\alber <albertoandradeortiz910@gmail.com>
2026-02-24 20:12:46 +0100

    BaseJpaEntity developed

--- src/main/java/com/albertoandrade/jiradepapel/common/infrastructure/persistence/BaseJpaEntity.java
+++ src/main/java/com/albertoandrade/jiradepapel/common/infrastructure/persistence/BaseJpaEntity.java
@@ -1,5 +1,44 @@
 package com.albertoandrade.jiradepapel.common.infrastructure.persistence;
 
-public class BaseJpaEntity {
-    
+import java.time.Instant;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    protected Instant createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    protected Instant updatedAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    protected String createdBy;
+
+    @LastModifiedBy
+    protected String lastModifiedBy;
 }
