a96efe09fa5f65f38f388b1ed4065ef515be1d2a
TOTI\alber <albertoandradeortiz910@gmail.com>
2026-02-24 20:27:26 +0100

    root user added developed

--- src/main/java/com/albertoandrade/jiradepapel/user/domain/model/User.java
+++ src/main/java/com/albertoandrade/jiradepapel/user/domain/model/User.java
@@ -1,5 +1,29 @@
 package com.albertoandrade.jiradepapel.user.domain.model;
 
+import java.util.Objects;
+
+import com.albertoandrade.jiradepapel.user.domain.model.valueObjects.UserId;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Root user added
+ */
+@Getter
+@Setter
 public class User {
-    
+
+    private final UserId id;
+    private String email;
+    private String password;
+    private UserRole userRole;
+
+    public User(UserId id, String email, String password, UserRole role) {
+        this.id = Objects.requireNonNull(id, "id cannot be null");
+        this.email = Objects.requireNonNull(email, "email cannot be null");
+        this.password = Objects.requireNonNull(password, "password cannot be null");
+        this.userRole = Objects.requireNonNull(userRole, "role cannot be null");
+    }
+
 }
