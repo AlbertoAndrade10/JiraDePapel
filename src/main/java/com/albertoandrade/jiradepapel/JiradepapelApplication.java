67ac1aeccbac4b8bc2a164eaae49ce67455f63a0
TOTI\alber <albertoandradeortiz910@gmail.com>
2026-02-24 19:23:48 +0100

    proyect added

---
+++ src/main/java/com/albertoandrade/jiradepapel/JiradepapelApplication.java
@@ -0,0 +1,13 @@
+package com.albertoandrade.jiradepapel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiradepapelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiradepapelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albertoandrade/jiradepapel/JiradepapelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albertoandrade.jiradepapel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiradepapelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
