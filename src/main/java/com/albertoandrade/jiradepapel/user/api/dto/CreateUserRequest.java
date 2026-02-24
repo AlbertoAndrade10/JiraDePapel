4f2ce1cc6152c9e97674a41471b801f458665353
TOTI\alber <albertoandradeortiz910@gmail.com>
2026-02-24 20:53:54 +0100

    CreateUserRequest and UserResponse developed

--- src/main/java/com/albertoandrade/jiradepapel/user/api/dto/CreateUserRequest.java
+++ src/main/java/com/albertoandrade/jiradepapel/user/api/dto/CreateUserRequest.java
@@ -1,5 +1,24 @@
 package com.albertoandrade.jiradepapel.user.api.dto;
 
+import com.albertoandrade.jiradepapel.user.domain.model.UserRole;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CreateUserRequest {
-    
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotNull
+    private UserRole role;
 }
--- src/main/java/com/albertoandrade/jiradepapel/user/api/dto/UserReponse.java
+++ src/main/java/com/albertoandrade/jiradepapel/user/api/dto/UserReponse.java
@@ -1,5 +1,23 @@
 package com.albertoandrade.jiradepapel.user.api.dto;
 
+import java.util.UUID;
+
+import com.albertoandrade.jiradepapel.user.domain.model.UserRole;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserReponse {
 
+    private UUID id;
+    private String email;
+    private UserRole role;
+
+    public UserReponse(UUID id, String email, UserRole role) {
+        this.id = id;
+        this.email = email;
+        this.role = role;
+    }
 }
